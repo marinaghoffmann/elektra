f890493c77d84fdf0353b888ff8556cf1ad7e85f
amanda-montarroios <amontarroiosoliveira@gmail.com>
2024-10-29 22:19:35 -0300

    ideia legal de estilo de titulo

--- src/main.c
+++ src/main.c
@@ -10,23 +10,48 @@ void telainicial() {
     screenClear();
 
     screenGotoxy(MINX + 5, MINY + 2);
-    screenSetColor(MAGENTA, BLACK);
-    printf("***** ELEKTRA *****\nProteja o planeta da tempestade de asteroides!");
+    screenSetColor(LIGHTCYAN, BLACK);
+    printf("███████╗██╗░░░░░███████╗██╗░░██╗████████╗██████╗░░█████╗░\n");
+
+    screenGotoxy(MINX + 5, MINY + 3);
+    screenSetColor(LIGHTCYAN, BLACK);
+    printf("██╔════╝██║░░░░░██╔════╝██║░██╔╝╚══██╔══╝██╔══██╗██╔══██╗\n");
 
     screenGotoxy(MINX + 5, MINY + 4);
+    screenSetColor(LIGHTCYAN, BLACK);
+    printf("█████╗░░██║░░░░░█████╗░░█████═╝░░░░██║░░░██████╔╝███████║\n");
+
+    screenGotoxy(MINX + 5, MINY + 5);
+    screenSetColor(LIGHTCYAN, BLACK);
+    printf("██╔══╝░░██║░░░░░██╔══╝░░██╔═██╗░░░░██║░░░██╔══██╗██╔══██║\n");
+
+    screenGotoxy(MINX + 5, MINY + 6);
+    screenSetColor(LIGHTCYAN, BLACK);
+    printf("███████╗███████╗███████╗██║░╚██╗░░░██║░░░██║░░██║██║░░██║\n");
+
+    screenGotoxy(MINX + 5, MINY + 7);
+    screenSetColor(LIGHTCYAN, BLACK);
+    printf("╚══════╝╚══════╝╚══════╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝\n");
+
+
+    screenGotoxy(MINX + 5, MINY + 10);
+    screenSetColor(BLUE, BLACK);
+    printf("Proteja o planeta da tempestade de asteroides!");
+
+    screenGotoxy(MINX + 5, MINY + 11);
     screenSetColor(BLUE, BLACK);
     printf("Pressione 'C' para começar ou 'S' para sair.");
 
-    screenGotoxy(MINX + 5, MINY + 6);
+    screenGotoxy(MINX + 5, MINY + 12);
     printf("Destrua os asteroides antes que eles atinjam a Terra.");
 
-    screenGotoxy(MINX + 5, MINY + 8);
+    screenGotoxy(MINX + 5, MINY + 13);
     printf("CUIDADO! Não seja atingido.");
 
-    screenGotoxy(MINX + 5, MINY + 10);
+    screenGotoxy(MINX + 5, MINY + 14);
     printf("Use as teclas de seta para mover a nave e espaço para atirar.");
 
-    screenGotoxy(MINX + 5, MINY + 12);
+    screenGotoxy(MINX + 5, MINY + 15);
     printf("Aperte 'C' para começar. Boa sorte!...");
 
     fflush(stdout);  
