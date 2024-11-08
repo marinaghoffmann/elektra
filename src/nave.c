2c9dc2416c59843b5a1d81985c16bce5c1c73d9d
Irvin Amilcar <165173581+IrvinAmilcar@users.noreply.github.com>
2024-11-08 13:08:02 -0300

    modificação nave.c
    O design e tamanho da nave foi modificado tornando-a mais compacta.

--- src/nave.c
+++ src/nave.c
@@ -24,21 +24,22 @@
 //}
 
 void desenharNave(int x, int y) {
-    /// desenha a nave na posição (x, y)
+    // Define as cores da nave
     screenSetColor(LIGHTCYAN, DARKGRAY);
 
-    screenGotoxy(x, y);     printf("   /\\   "); //inicio da posição escolhida na main
-    screenGotoxy(x, y + 1); printf("  /  \\  "); //a posição escolhida +1
-    screenGotoxy(x, y + 2); printf(" /    \\ "); // e assim por diante...
-    screenGotoxy(x, y + 3); printf(" |    | ");  // AMANDA: vou melhorar essa nave, ta mt feia né? 
-    screenGotoxy(x, y + 4); printf(" \\____/ ");
-    
-    screenSetNormal(); // setar a tela para o "normal"
+    // Nave redesenhada um pouco menor
+    screenGotoxy(x, y);     printf("  ^  "); //começa na posição inicial e vai somando mais 1 sucetivamente
+    screenGotoxy(x, y + 1); printf(" /_\\ ");
+    screenGotoxy(x, y + 2); printf("|_o_|");
+
+    // Restaura a cor padrão da tela
+    screenSetNormal();
 }
+
 
 void apagaNave(int x, int y) {
     // apaga a nave desenhando espaços nas posições da nave para que der a ideia de movimento
-    for (int i = 0; i < 5; i++) { // 5 já que a nave ocupa 5 linhas, para que todas se movam
+    for (int i = 0; i < 3; i++) { // 5 já que a nave ocupa 5 linhas, para que todas se movam
         screenGotoxy(x, y + i); //move o cursor para essas posições
         printf("        "); 
     }
@@ -64,7 +65,7 @@ void moverNave() {
             if (tecla == 91) { // verifica a sequência que indica que é uma seta, para que seja lido como seta
                 tecla = getchar();  // lê o próximo caractere da sequência
 
-                if (tecla == 67 && x < MAXX - 7) x++; // seta para a direita e verifica se a nave esta dentro do limite da tela
+                if (tecla == 67 && x < MAXX - 5) x++; // seta para a direita e verifica se a nave esta dentro do limite da tela (Houve uma modificação nessa linha para o novo tamanho da nave (de 7 foi para 5))
                 else if (tecla == 68 && x > MINX) x--;    // seta para a esquerda e verifica se a nave esta dentro do limite da tela
             }
         }
